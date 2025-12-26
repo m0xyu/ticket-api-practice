cd703aac879138b6b7be2a623ccfce661fe67a7d
xaseki <you@example.com>
2025-12-26 18:30:23 +0900

    Event、Reservationモデルを作成

---
+++ app/Models/Reservation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Reservation extends Model
+{
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'reserved_at',
+    ];
+
+    protected $casts = [
+        'event_id' => 'integer',
+        'user_id' => 'integer',
+        'reserved_at' => 'datetime'
+    ];
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
+}
